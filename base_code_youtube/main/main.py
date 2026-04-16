9461f12f169bdf72c209e6676dbf40fd6ffcb0dd
amer872278-cpu <amer872278@gmail.com>
2026-04-16 20:56:18 +0200

    Add files via upload

--- base_code_youtube/main/main.py
+++ base_code_youtube/main/main.py
@@ -1,6 +1,8 @@
+###NOTE: When running this program, make sure in the terminal that you're running it from the folder containing main.py!
+
 # import python built in libraries
 from sys import path
-path.append('C:\\Users\\amer8\\OneDrive\\Dokument\\R1\\Code\\python_tools') ###CHANGE THIS TO YOUR OWN PATH
+path.append('C:\\Users\\amer8\\OneDrive\\Dokument\\R1\\Code\\python_tools') ###Change this to your own path!
 
 # 3rd party imports
 import numpy as np
@@ -17,40 +19,44 @@ import tools as t
 import spice_tools as st
 
 # time parameters
-tspan=3600*24*365.0 # 1 days
+tspan=3600*24*14.0 # 1 days
 dt=5000
 
 # central body
 cb = pd.earth
 
-date0='2020-03-08'
+date0='2027-01-01'
 
 h=30.0e-3 # km
 w=35e-3 # km
-A=h*w # km^2
+A=4*10**(0-6) # km^2
 
 if __name__ == '__main__':
 
     # initial conditions
-    state1=[42095.0,0.81818,28.5,180.0,298.2253,357.857]
-    state0=t.tle2coes('geo.txt')
+    state0=[500.0+cb['radius'],0.0,0.0,0.0,7.66,0.0]
 
     # null perturbations dictionary
     perts=null_perts()
 
-    # add lunar gravity perturbation
+    # add solar pressure radiation
     perts['srp']=True
-    perts['A_srp']=A
+    perts['A_srp']=A/2
     perts['CR']=1.0
-    mass0=7000.0 # kg
+
+    # add aerodynamic drag
+    perts['aero']=True
+    perts['Cd']=2.2
+    perts['A']=A/2 # km^2
+
+    mass0=0.150 # kg
 
     # create orbit propagator instance
-    op0=OP(state0,tspan,1000,coes=True,deg=True,perts=perts,date0=date0,propagator='dopri5',mass0=mass0)
-    op0.plot_3d(save_plot=True,title='Geostationary Orbit')
-    op0.calculate_coes()
-    op0.plot_coes(days=True,save_plot=True,rel=True,title='Geostationary')
+    op0=OP(state0,tspan,1000,coes=False,deg=True,perts=perts,date0=date0,propagator='dopri5',mass0=mass0)
+    #op0.plot_3d(show_plot=True,title='Solar Sail Orbit')
+    #op0.calculate_coes()
+    #op0.plot_coes(days=True,show_plot=True,rel=False,title='Solar Sail')
+    op0.plot_alts(show_plot=True,hours=True)
 
-    op1=OP(state1,tspan,1000,coes=True,deg=True,perts=perts,date0=date0,propagator='dopri5',mass0=mass0)
-    op1.plot_3d(save_plot=True,title='Molniya Orbit')
-    op1.calculate_coes()
-    op1.plot_coes(days=True,save_plot=True,rel=True,title='Molniya')
+
+
